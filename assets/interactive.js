160cac8a285bd2d0f49b11ff0dc7745738461fc8
tubaShawn <shawnrmangum@gmail.com>
2019-05-23 22:26:45 -0600

    added code pen logic from work computer

---
+++ assets/interactive.js
@@ -0,0 +1,69 @@
+
+
+// let submitButton = document.getElementById("submit-button"),
+//     output = document.getElementById("output"),
+//     numberInput = document.getElementById("number-input"),
+//     minutes = [1, 2, 3, 4, 5];
+
+
+
+// submitButton.addEventListener("click", function() {
+//     alert("This worked");
+// });
+
+
+// let i = 0;
+// let minuteChanger = minutes[i];
+// if(output.textContent === "Test") {
+//     output.innerText = minuteChanger;
+//     i++;
+// } else if(output.textContent === minutes[4]) {
+//     i = 0;
+//     output.innerText = minuteChanger;
+// } else {
+//     i++;
+//     output.innerText = minuteChanger;
+// }
+
+let ballQueue = [];
+let input = document.getElementById("input");
+let submit = document.getElementById("submit");
+let output = document.getElementById("empty-div");
+let nextOutput = document.getElementById("next-div");
+
+submit.addEventListener("click", function() {
+  // output.textContent = "You entered " + input.value;
+    arrayMaker();
+    output.textContent = "You have put " + ballQueue.length + " balls into your clock";
+    nextOutput.textContent = ballQueue[0];
+    clock();
+});
+
+function arrayMaker() {
+    let temp = input.value;
+    for (let i = 0; i < temp; i++) {
+    ballQueue.push(i);
+    } 
+}
+
+function shifter() {
+    let temp = ballQueue.shift();
+    console.log(temp);
+}
+
+function clock() {
+    ballQueue;
+    let singles = [];
+    let fives = [];
+    let hours = [];
+    let days = 0;
+    let addFive = singles.shift();
+    let addHour = fives.shift();
+    for (let i = 0; i < 5; i++) {
+    let addSingle = ballQueue.shift();
+    singles[i] = addSingle;
+    console.log(ballQueue.length + " + " + singles.length);
+    // console.log(singles);
+    }
+
+}
